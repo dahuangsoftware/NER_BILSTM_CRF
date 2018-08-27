189ff38981c4dbbf3416b1eb49f5d9646a58690e
unknown <huangxin_sx@iqiyi.pps>
2018-08-27 20:56:34 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,123 @@
+import tensorflow as tf
+import numpy as np
+import os, argparse, time, random
+from model import BiLSTM_CRF
+from utils import str2bool, get_logger, get_entity
+from data import read_corpus, read_dictionary, tag2label, random_embedding
+
+## Session configuration
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # default: 0
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.2  # need ~700MB GPU memory
+
+## hyperparameters
+#一些运行时输入的参数
+parser = argparse.ArgumentParser(description='BiLSTM-CRF for Chinese NER task')
+parser.add_argument('--train_data', type=str, default='data_path', help='train data source')
+parser.add_argument('--test_data', type=str, default='data_path', help='test data source')
+parser.add_argument('--batch_size', type=int, default=64, help='#sample of each minibatch')#小批量的样本数，训练用到多少个batch，即mini-batch的数量
+parser.add_argument('--epoch', type=int, default=40, help='#epoch of training')#向前和向后传播中所有批次的单次训练迭代
+parser.add_argument('--hidden_dim', type=int, default=300, help='#dim of hidden state')
+parser.add_argument('--optimizer', type=str, default='Adam', help='Adam/Adadelta/Adagrad/RMSProp/Momentum/SGD')
+parser.add_argument('--CRF', type=str2bool, default=True, help='use CRF at the top layer. if False, use Softmax')#是否使用CRF层
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--clip', type=float, default=5.0, help='gradient clipping')
+parser.add_argument('--dropout', type=float, default=0.5, help='dropout keep_prob')
+parser.add_argument('--update_embedding', type=str2bool, default=True, help='update embedding during training')#在培训期间更新嵌入
+parser.add_argument('--pretrain_embedding', type=str, default='random', help='use pretrained char embedding or init it randomly')#预训练或者随机初始化
+parser.add_argument('--embedding_dim', type=int, default=300, help='random init char embedding_dim')
+parser.add_argument('--shuffle', type=str2bool, default=True, help='shuffle training data before each epoch')#在每次迭代之前更改训练数据
+parser.add_argument('--mode', type=str, default='demo', help='train/test/demo')#
+parser.add_argument('--demo_model', type=str, default='1521112368', help='model for test and demo')#用途测试和demo的训练模型
+args = parser.parse_args()
+
+
+## get char embeddings
+word2id = read_dictionary(os.path.join('.', args.train_data, 'word2id.pkl'))#反序列化，返回字和对应的
+#print(word2id)
+if args.pretrain_embedding == 'random':
+    print("随机初始化")
+    embeddings = random_embedding(word2id, args.embedding_dim)#随机初始化的embedding矩阵
+    print(embeddings)
+else:
+    embedding_path = 'pretrain_embedding.npy'
+    embeddings = np.array(np.load(embedding_path), dtype='float32')#经过训练的embedding矩阵
+
+
+## read corpus and get training data
+if args.mode != 'demo':
+    train_path = os.path.join('.', args.train_data, 'train_data')
+    test_path = os.path.join('.', args.test_data, 'test_data')
+    train_data = read_corpus(train_path)
+    test_data = read_corpus(test_path); test_size = len(test_data)
+
+
+## paths setting
+paths = {}
+timestamp = str(int(time.time())) if args.mode == 'train' else args.demo_model
+output_path = os.path.join('.', args.train_data+"_save", timestamp)
+if not os.path.exists(output_path): os.makedirs(output_path)
+summary_path = os.path.join(output_path, "summaries")
+paths['summary_path'] = summary_path
+if not os.path.exists(summary_path): os.makedirs(summary_path)
+model_path = os.path.join(output_path, "checkpoints/")
+if not os.path.exists(model_path): os.makedirs(model_path)
+ckpt_prefix = os.path.join(model_path, "model")
+paths['model_path'] = ckpt_prefix
+result_path = os.path.join(output_path, "results")
+paths['result_path'] = result_path
+if not os.path.exists(result_path): os.makedirs(result_path)
+log_path = os.path.join(result_path, "log.txt")
+paths['log_path'] = log_path
+get_logger(log_path).info(str(args))
+
+
+## training model
+if args.mode == 'train':
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)
+    model.build_graph()
+    ## hyperparameters-tuning, split train/dev
+    # dev_data = train_data[:5000]; dev_size = len(dev_data)
+    # train_data = train_data[5000:]; train_size = len(train_data)
+    # print("train data: {0}\ndev data: {1}".format(train_size, dev_size))
+    # model.train(train=train_data, dev=dev_data)
+    ## train model on the whole training data
+    print("train data: {}".format(len(train_data)))
+    model.train(train=train_data, dev=test_data)  # use test_data as the dev_data to see overfitting phenomena
+
+## test
+elif args.mode == 'test':
+    ckpt_file = tf.train.latest_checkpoint(model_path)
+    print(ckpt_file)
+    paths['model_path'] = ckpt_file
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)
+    model.build_graph()
+    print("test data: {}".format(test_size))
+    model.test(test_data)
+
+## 测试
+elif args.mode == 'demo':
+    ckpt_file = tf.train.latest_checkpoint(model_path)#自动获取保存的最新的一次模型
+    print(ckpt_file)
+    paths['model_path'] = ckpt_file
+    model = BiLSTM_CRF(args, embeddings, tag2label, word2id, paths, config=config)#初始化模型
+    model.build_graph()
+    saver = tf.train.Saver()#创建一个saver对象
+    with tf.Session(config=config) as sess:
+        print('============= demo =============')
+        saver.restore(sess, ckpt_file)#模型的恢复
+        while(1):
+            print('Please input your sentence:')
+            demo_sent = input()
+            if demo_sent == '' or demo_sent.isspace():
+                print('See you next time!')
+                break
+            else:
+                demo_sent = list(demo_sent.strip())
+                demo_data = [(demo_sent, ['O'] * len(demo_sent))]
+                tag = model.demo_one(sess, demo_data)
+                #获取其中的人名，地名，组织机构名
+                PER, LOC, ORG = get_entity(tag, demo_sent)
+                print('PER: {}\nLOC: {}\nORG: {}'.format(PER, LOC, ORG))
---
+++ model.py
@@ -0,0 +1,325 @@
+import numpy as np
+import os, time, sys
+import tensorflow as tf
+from tensorflow.contrib.rnn import LSTMCell
+from tensorflow.contrib.crf import crf_log_likelihood
+from tensorflow.contrib.crf import viterbi_decode
+from data import pad_sequences, batch_yield
+from utils import get_logger
+from eval import conlleval
+
+
+class BiLSTM_CRF(object):
+    def __init__(self, args, embeddings, tag2label, vocab, paths, config):
+        self.batch_size = args.batch_size
+        self.epoch_num = args.epoch
+        self.hidden_dim = args.hidden_dim
+        self.embeddings = embeddings
+        self.CRF = args.CRF
+        self.update_embedding = args.update_embedding
+        self.dropout_keep_prob = args.dropout
+        self.optimizer = args.optimizer
+        self.lr = args.lr
+        self.clip_grad = args.clip
+        self.tag2label = tag2label
+        self.num_tags = len(tag2label)
+        self.vocab = vocab
+        self.shuffle = args.shuffle
+        self.model_path = paths['model_path']
+        self.summary_path = paths['summary_path']
+        self.logger = get_logger(paths['log_path'])
+        self.result_path = paths['result_path']
+        self.config = config
+
+    def build_graph(self):
+        self.add_placeholders()
+        self.lookup_layer_op()
+        self.biLSTM_layer_op()
+        self.softmax_pred_op()
+        self.loss_op()
+        self.trainstep_op()
+        self.init_op()
+
+    # 用户给变量定义过程，该操作提供占位符
+    def add_placeholders(self):
+        self.word_ids = tf.placeholder(tf.int32, shape=[None, None], name="word_ids")
+        self.labels = tf.placeholder(tf.int32, shape=[None, None], name="labels")
+        self.sequence_lengths = tf.placeholder(tf.int32, shape=[None], name="sequence_lengths")
+
+        self.dropout_pl = tf.placeholder(dtype=tf.float32, shape=[], name="dropout")
+        self.lr_pl = tf.placeholder(dtype=tf.float32, shape=[], name="lr")
+    #look-up 层，利用预训练或随机初始化的embedding矩阵将句子中的每个字 xi 由
+    # one-hot向量映射为低维稠密的字向量（character embedding）xi∈Rd ，
+    # d 是embedding的维度。在输入下一层之前，设置dropout以缓解过拟合。
+    def lookup_layer_op(self):
+        with tf.variable_scope("words"):#命名空间，实现变量共享的功能 ，跟 tf.get_variable() 配合使用，实现变量共享的功能。
+            _word_embeddings = tf.Variable(self.embeddings,
+                                           dtype=tf.float32,
+                                           trainable=self.update_embedding,
+                                           name="_word_embeddings")
+            word_embeddings = tf.nn.embedding_lookup(params=_word_embeddings, #embedding_lookup选取一个张量里面索引对应的元素
+                                                     ids=self.word_ids,
+                                                     name="word_embeddings")
+        self.word_embeddings =  tf.nn.dropout(word_embeddings, self.dropout_pl)#防止出现过拟合
+    #第二层是双向LSTM层，自动提取句子特征
+    def biLSTM_layer_op(self):
+        with tf.variable_scope("bi-lstm"):
+            #LSTM循环神经网络单元
+            cell_fw = LSTMCell(self.hidden_dim)#前向
+            cell_bw = LSTMCell(self.hidden_dim)#后向
+            #双向RNN，实现双向LSTM
+            (output_fw_seq, output_bw_seq), _ = tf.nn.bidirectional_dynamic_rnn(
+                cell_fw=cell_fw,
+                cell_bw=cell_bw,
+                inputs=self.word_embeddings,
+                sequence_length=self.sequence_lengths,
+                dtype=tf.float32)
+            #连接两个矩阵
+            output = tf.concat([output_fw_seq, output_bw_seq], axis=-1)
+            output = tf.nn.dropout(output, self.dropout_pl)
+        #接入一个线性层，将隐状态向量从m维映射到k维，k是标注集的标签数，Pij表示字xi分到第j个标签的打分
+        with tf.variable_scope("proj"):
+            W = tf.get_variable(name="W",
+                                shape=[2 * self.hidden_dim, self.num_tags],
+                                initializer=tf.contrib.layers.xavier_initializer(),
+                                dtype=tf.float32)
+
+            b = tf.get_variable(name="b",
+                                shape=[self.num_tags],
+                                initializer=tf.zeros_initializer(),
+                                dtype=tf.float32)
+            #获得变量的shape
+            s = tf.shape(output)
+            #将tensor变换为参数shape的形式
+            output = tf.reshape(output, [-1, 2*self.hidden_dim])
+            #矩阵点乘
+            pred = tf.matmul(output, W) + b
+
+            self.logits = tf.reshape(pred, [-1, s[1], self.num_tags])
+    #CRF层，进行句子级别的序列标注
+    def loss_op(self):
+        if self.CRF:
+            #使用crf 来计算损失，里面用到的优化方法是：最大似然估计
+            log_likelihood, self.transition_params = crf_log_likelihood(inputs=self.logits,
+                                                                   tag_indices=self.labels,
+                                                                   sequence_lengths=self.sequence_lengths)
+            #reduce_mean：沿着tensor的某一维度，计算元素的平均值。由于输出tensor的维度比原tensor的低，这类操作也叫降维
+            self.loss = -tf.reduce_mean(log_likelihood)
+        else:
+            #将softmax和cross_entropy放在一起计算，计算交叉熵
+            losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits,
+                                                                    labels=self.labels)
+            #返回一个表示每个单元的前N个位置的mask张量
+            #构建序列长度的mask标志
+            mask = tf.sequence_mask(self.sequence_lengths)
+            #使a (m维)矩阵仅保留与b中“True”元素同下标的部分，并将结果展开到m-1维
+            losses = tf.boolean_mask(losses, mask)
+            self.loss = tf.reduce_mean(losses)#在某一维上求取平均值
+        #输出学习速度和期望误差
+        tf.summary.scalar("loss", self.loss)
+    #softmax层，CRF为false时使用
+    def softmax_pred_op(self):
+        if not self.CRF:
+            self.labels_softmax_ = tf.argmax(self.logits, axis=-1)
+            self.labels_softmax_ = tf.cast(self.labels_softmax_, tf.int32)
+
+    def trainstep_op(self):
+        with tf.variable_scope("train_step"):
+            self.global_step = tf.Variable(0, name="global_step", trainable=False)
+            if self.optimizer == 'Adam':
+                #寻找全局最优点的优化算法
+                optim = tf.train.AdamOptimizer(learning_rate=self.lr_pl)
+            elif self.optimizer == 'Adadelta':
+                optim = tf.train.AdadeltaOptimizer(learning_rate=self.lr_pl)
+            elif self.optimizer == 'Adagrad':
+                optim = tf.train.AdagradOptimizer(learning_rate=self.lr_pl)
+            elif self.optimizer == 'RMSProp':
+                optim = tf.train.RMSPropOptimizer(learning_rate=self.lr_pl)
+            elif self.optimizer == 'Momentum':
+                optim = tf.train.MomentumOptimizer(learning_rate=self.lr_pl, momentum=0.9)
+            elif self.optimizer == 'SGD':
+                optim = tf.train.GradientDescentOptimizer(learning_rate=self.lr_pl)
+            else:
+                optim = tf.train.GradientDescentOptimizer(learning_rate=self.lr_pl)
+            #计算`var_list`中变量`loss`的梯度
+            grads_and_vars = optim.compute_gradients(self.loss)
+            grads_and_vars_clip = [[tf.clip_by_value(g, -self.clip_grad, self.clip_grad), v] for g, v in grads_and_vars]
+            #应用梯度
+            self.train_op = optim.apply_gradients(grads_and_vars_clip, global_step=self.global_step)
+
+    def init_op(self):
+        self.init_op = tf.global_variables_initializer()
+
+    def add_summary(self, sess):
+        """
+        :param sess:
+        :return:
+        """
+        self.merged = tf.summary.merge_all()#将之前定义的所有summary op整合到一起，保存在磁盘
+        self.file_writer = tf.summary.FileWriter(self.summary_path, sess.graph)#指定一个文件用来保存图
+
+    def train(self, train, dev):
+        """
+
+        :param train:
+        :param dev:
+        :return:
+        """
+        #创建一个saver对象
+        saver = tf.train.Saver(tf.global_variables())
+
+        with tf.Session(config=self.config) as sess:
+            sess.run(self.init_op)# 初始化模型的参数
+            self.add_summary(sess)
+
+            for epoch in range(self.epoch_num):
+                self.run_one_epoch(sess, train, dev, self.tag2label, epoch, saver)
+
+    def test(self, test):
+        saver = tf.train.Saver()
+        with tf.Session(config=self.config) as sess:
+            self.logger.info('=========== testing ===========')
+            saver.restore(sess, self.model_path)
+            label_list, seq_len_list = self.dev_one_epoch(sess, test)
+            self.evaluate(label_list, seq_len_list, test)
+    #生成label标签
+    def demo_one(self, sess, sent):
+        """
+        :param sess:
+        :param sent: 
+        :return:
+        """
+        label_list = []
+        for seqs, labels in batch_yield(sent, self.batch_size, self.vocab, self.tag2label, shuffle=False):
+            label_list_, _ = self.predict_one_batch(sess, seqs)
+            label_list.extend(label_list_)
+        label2tag = {}
+        for tag, label in self.tag2label.items():
+            label2tag[label] = tag if label != 0 else label
+        tag = [label2tag[label] for label in label_list[0]]
+        return tag
+    #对每一个epoch进
+    def run_one_epoch(self, sess, train, dev, tag2label, epoch, saver):
+        """
+        :param sess:
+        :param train:
+        :param dev:
+        :param tag2label:
+        :param epoch:
+        :param saver:
+        :return:
+        """
+        num_batches = (len(train) + self.batch_size - 1) // self.batch_size
+
+        start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        batches = batch_yield(train, self.batch_size, self.vocab, self.tag2label, shuffle=self.shuffle)
+        for step, (seqs, labels) in enumerate(batches):
+
+            sys.stdout.write(' processing: {} batch / {} batches.'.format(step + 1, num_batches) + '\r')
+            step_num = epoch * num_batches + step + 1
+            feed_dict, _ = self.get_feed_dict(seqs, labels, self.lr, self.dropout_keep_prob)
+            _, loss_train, summary, step_num_ = sess.run([self.train_op, self.loss, self.merged, self.global_step],
+                                                         feed_dict=feed_dict)
+            if step + 1 == 1 or (step + 1) % 300 == 0 or step + 1 == num_batches:
+                self.logger.info(
+                    '{} epoch {}, step {}, loss: {:.4}, global_step: {}'.format(start_time, epoch + 1, step + 1,
+                                                                                loss_train, step_num))
+
+            self.file_writer.add_summary(summary, step_num)
+
+            if step + 1 == num_batches:
+                saver.save(sess, self.model_path, global_step=step_num)
+
+        self.logger.info('===========validation / test===========')
+        label_list_dev, seq_len_list_dev = self.dev_one_epoch(sess, dev)
+        self.evaluate(label_list_dev, seq_len_list_dev, dev, epoch)
+
+    def get_feed_dict(self, seqs, labels=None, lr=None, dropout=None):
+        """
+
+        :param seqs:
+        :param labels:
+        :param lr:
+        :param dropout:
+        :return: feed_dict
+        """
+        word_ids, seq_len_list = pad_sequences(seqs, pad_mark=0)
+
+        feed_dict = {self.word_ids: word_ids,
+                     self.sequence_lengths: seq_len_list}
+        if labels is not None:
+            labels_, _ = pad_sequences(labels, pad_mark=0)
+            feed_dict[self.labels] = labels_
+        if lr is not None:
+            feed_dict[self.lr_pl] = lr
+        if dropout is not None:
+            feed_dict[self.dropout_pl] = dropout
+
+        return feed_dict, seq_len_list
+
+    def dev_one_epoch(self, sess, dev):
+        """
+        :param sess:
+        :param dev:
+        :return:
+        """
+        label_list, seq_len_list = [], []
+        for seqs, labels in batch_yield(dev, self.batch_size, self.vocab, self.tag2label, shuffle=False):
+            label_list_, seq_len_list_ = self.predict_one_batch(sess, seqs)
+            label_list.extend(label_list_)
+            seq_len_list.extend(seq_len_list_)
+        return label_list, seq_len_list
+
+    def predict_one_batch(self, sess, seqs):
+        """
+
+        :param sess:
+        :param seqs:
+        :return: label_list
+                 seq_len_list
+        """
+        feed_dict, seq_len_list = self.get_feed_dict(seqs, dropout=1.0)
+
+        if self.CRF:
+            logits, transition_params = sess.run([self.logits, self.transition_params],
+                                                 feed_dict=feed_dict)
+            label_list = []
+            for logit, seq_len in zip(logits, seq_len_list):
+                viterbi_seq, _ = viterbi_decode(logit[:seq_len], transition_params)
+                label_list.append(viterbi_seq)
+            return label_list, seq_len_list
+
+        else:
+            label_list = sess.run(self.labels_softmax_, feed_dict=feed_dict)
+            return label_list, seq_len_list
+
+    def evaluate(self, label_list, seq_len_list, data, epoch=None):
+        """
+
+        :param label_list:
+        :param seq_len_list:
+        :param data:
+        :param epoch:
+        :return:
+        """
+        label2tag = {}
+        for tag, label in self.tag2label.items():
+            label2tag[label] = tag if label != 0 else label
+
+        model_predict = []
+        for label_, (sent, tag) in zip(label_list, data):
+            tag_ = [label2tag[label__] for label__ in label_]
+            sent_res = []
+            if  len(label_) != len(sent):
+                print(sent)
+                print(len(label_))
+                print(tag)
+            for i in range(len(sent)):
+                sent_res.append([sent[i], tag[i], tag_[i]])
+            model_predict.append(sent_res)
+        epoch_num = str(epoch+1) if epoch != None else 'test'
+        label_path = os.path.join(self.result_path, 'label_' + epoch_num)
+        metric_path = os.path.join(self.result_path, 'result_metric_' + epoch_num)
+        for _ in conlleval(model_predict, label_path, metric_path):
+            self.logger.info(_)
+
